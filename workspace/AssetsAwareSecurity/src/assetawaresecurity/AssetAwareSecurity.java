568e22aa04a3a8aa8ee53fa3bc06feaad618a4bc
Methisz <zinonos.mario@gmail.com>
2015-12-10 04:00:32 +0000

    Adding lib internally

--- workspace/AssetsAwareSecurity/src/assetawaresecurity/AssetAwareSecurity.java
+++ workspace/AssetsAwareSecurity/src/assetawaresecurity/AssetAwareSecurity.java
@@ -35,12 +35,7 @@ public class AssetAwareSecurity {
 	}
 
 	public static void main(String[] args) throws IOException, CloudmeException {
-<<<<<<< HEAD
 
-// Change this to see what happens
-=======
->>>>>>> branch 'master' of https://github.com/GiannisT/AssetsAwareSecurity.git
-		
 		//this thread discovers the OS of the system and the upload speed
 		new Thread()
 		{
